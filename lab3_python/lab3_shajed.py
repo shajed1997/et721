efb8209851a5aad01065f1afaa926e2017b2d1bb
shajed1997 <99819896+shajed1997@users.noreply.github.com>
2024-09-11 07:56:40 -0400

    lab3_shajed.py

--- lab3_python/lab3_shajed.py
+++ lab3_python/lab3_shajed.py
@@ -36,7 +36,57 @@ print(f"Your final grade for the class is {finalgrade} ={gpa}")
          gpa ='UNDEFINED'  
 
 
- PRINT(F"YOUR FINAL GRADE FOR THE CLASS IS {finalgrade}={gpa}")
+
+ print  ("-----example 2: Loops ---------")
+SECRET = 8
+userguess = int (input("Gues a  number between 1 and 10: "))
+while not (SECRET ==userguess):
+    usergueess = int(input ("wrong guess. guess again:"))
+
+
+print (f"Congrats! {userguess} is the right number!")
+print("-------examole :3 Loops, break statement ---------")
+blance =1000
+widthdraw =0
+deposit =0
+while True:
+    userinput =input("Do you want to widthdraw, w, or deposit, d?")
+    if userinput == 'w' or userinput =='w':
+        w_amount = int(input('How much do you want to widthdraw?'))
+        if w_amount>balance:
+            print(f"unsuficient funds! you can't widthdraw more than{balance}")
+        else:
+            balance -= w_amount    
+
+
+    elif  userinput == 'd' or userinput =='D':
+       
+          d_amount = int (input('How much do you want to deposit?'))
+          balance += d_amount
+          print(f"your new balance is {balance}")  
+    else:
+        print("Invalid selection!")  
+    choice = Input ("would you like to do another transaction?(y,n)")  
+    if not(choice=='y' or choce== 'Y')  
+       break
+print("Thank you for banking with us!")  
+
+
+print("-----example 4: for loop as a counter --------")
+
+
+for n in range(-5,3,2):
+    print(f"counting = {n}")
+
+
+print ("-----example 5: for loop in a list")  
+colours = ['magenta','babyblue';'olive']
+
+
+for c in colors:
+    print(f"color = {c}")
+
+
 
 
 
